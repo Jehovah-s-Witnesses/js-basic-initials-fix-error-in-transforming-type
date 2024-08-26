25c5035c632c41ca1d291ce466e193a83c08c4b7
gribVolnushka <burgerqueen98@gmail.com>
2024-08-26 18:40:52 +0200

    fixed error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-let userName = prompt("What's your name?")
-
-let age = prompt("How old are you?")
-
-let ageInFiveYears = age + 5
+let userName = prompt("What's your name?");
+let age = +prompt("How old are you?");
+let ageInFiveYears = age + 5;
 
 if (ageInFiveYears === 30) {
-  alert(userName + ", you will be 30 years old in 5 years!")
+  alert(`${userName} , you will be 30 years old in 5 years!`);
 } else {
-  alert(userName + ", you will be " + ageInFiveYears + " years old in 5 years.")
+  alert(`${userName} , you will be ${ageInFiveYears} years old in 5 years.`);
 }
