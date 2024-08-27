4f7fcad233e7b30b4e6b4c21356eb576be123313
gribVolnushka <burgerqueen98@gmail.com>
2024-08-27 16:03:16 +0200

    changed types of variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-let userName = prompt("What's your name?");
-let age = +prompt("How old are you?");
-let ageInFiveYears = age + 5;
+const userName = prompt("What's your name?");
+const age = +prompt('How old are you?');
+const ageInFiveYears = age + 5;
 
 if (ageInFiveYears === 30) {
   alert(`${userName} , you will be 30 years old in 5 years!`);
